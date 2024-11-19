23715e2cd70a3f9b4677de80adc7251399a62b27
narim <0018823nmn@naver.com>
2024-11-20 04:33:38 +0900

    24-11-19

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Image from "next/image";
+// import Image from "next/image";
 import { CardBoard } from "@/features";
 import { Button, SearchBar, Progress, LabelDatePicker } from "@/components/ui";
 import { ChevronLeft } from "lucide-react";
