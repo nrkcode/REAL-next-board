139eff0fca5aa5432dd95b864cb329fac8f02a35
narim <0018823nmn@naver.com>
2024-11-20 03:52:50 +0900

    코드정리

--- features/board/CardBoard.tsx
+++ features/board/CardBoard.tsx
@@ -1,18 +1,7 @@
-import {
-    Button,
-    Card,
-    Checkbox,
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-    LabelDatePicker,
-    Separator,
-} from "@/components/ui";
+"use client";
+import { Button, Card, Checkbox, Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger, LabelDatePicker, Separator } from "@/components/ui";
 import { ChevronUp } from "lucide-react";
+
 function CardBoard() {
     return (
         <Card className="w-full flex flex-col items-center p-5">
@@ -20,12 +9,7 @@ function CardBoard() {
             <div className="w-full flex items-center justify-between mb-4">
                 <div className="flex items-center justify-start gap-2">
                     <Checkbox className="h-5 w-5" />
-                    <input
-                        type="text"
-                        placeholder="제목을 입력하세요."
-                        className="text-xl outline-none"
-                        disabled={true}
-                    />
+                    <input type="text" placeholder="제목 없음." className="text-xl outline-none bg-transparent" disabled={true} />
                 </div>
                 <Button variant={"ghost"} size={"icon"}>
                     <ChevronUp className="text-[#6d6d6d]" />
@@ -56,9 +40,26 @@ function CardBoard() {
             </div>
             <Separator className="my-3" />
             {/* Add Contents 버튼 영역 */}
-            <Button variant={"ghost"} className="font-normal text-[#6D6D6D]">
-                Add Contents
-            </Button>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button
+                        variant={"ghost"}
+                        className="font-normal text-[#6D6D6D]"
+                    >
+                        Add Contents
+                    </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>Make changes to your profile here. Click save when you're done.</DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4"></div>
+                    <DialogFooter>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
         </Card>
     );
 }
