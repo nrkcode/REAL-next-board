1ab3349a4a9b3ac08ccdefab0ad19afdaf6855bc
narim <0018823nmn@naver.com>
2024-11-20 01:08:39 +0900

    feat: 새페이지 헤더 ui

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -1,35 +1,27 @@
-import { Button, SearchBar } from "@/components/ui";
+import { Input, Button, SearchBar, Progress } from "@/components/ui";
+import styles from "./page.module.scss";
 
 function BoardPage() {
-  return (
-    <div className="page">
-            <aside className="page__aside">
-                {/* 검색창 ui */}
-                <SearchBar placeholder="검색어를 입력하세요."></SearchBar>
-                {/* add new page 버튼 ui */}
-                <Button className="text-[#E79057] bg-white border border-[#E79057] hover:bg-[#FFF9F5]">
-                    Add New Page
-                </Button>
-                {/* todo 목록 ui 1개 */}
-                <div className="flex flex-col mt-4 gap-1">
-                    <small className="text-sm font-medium leading-none text-[#a6a6a6]">
-                        고마11의 TODO-board
-                    </small>
-                    <ul className="flex flex-col">
-                        <li className="flex items-center py-2 px-[10px] gap-2 bg-[#f5f5f5]">
-                            <div className="h-[6px] w-[6px] bg-green-300 rounded-full"></div>
-                            리스트
-                        </li>
-                        <li className="flex items-center py-2 px-[10px] gap-2 bg-[#f5f5f5]">
-                            <div className="h-[6px] w-[6px] bg-green-300 rounded-full"></div>
-                            리스트
-                        </li>
-                    </ul>
+    return (
+        <div className="page">
+            <aside className="page__aside"></aside>
+            <main className="page__main">
+                <div className={styles.header}>
+                    {/* 제목 입력 Input 섹션 */}
+                    <Input placeholder="Enter Title Here!" />
+                    {/* 진행상황 척도 그래프 섹션 */}
+                    <div className="flex items-center justify-start gap-4">
+                        <small className="text-sm font-medium leading-none text-[#6D6D6D]">
+                            1/10 Completed
+                        </small>
+                        <Progress className="w-60 h-[10px]" />
+                    </div>
                 </div>
-            </aside>
-            <main className="page__main"></main>
+                <div>{/* 캘린더 + Add New Board 버튼 섹션 */}</div>
+                <div className={styles.body}></div>
+            </main>
         </div>
-  )
+    );
 }
 
 export default BoardPage;
