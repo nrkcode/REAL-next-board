704c8b025a91b156598f974296e178a579b65fcb
narim <0018823nmn@naver.com>
2024-11-20 01:11:20 +0900

    feat: 보드 aside 복붙

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -4,7 +4,30 @@ import styles from "./page.module.scss";
 function BoardPage() {
     return (
         <div className="page">
-            <aside className="page__aside"></aside>
+            <aside className="page__aside">
+                {/* 검색창 UI */}
+                <SearchBar placeholder="검색어를 입력하세요." />
+                {/* Add New Page 버튼 UI */}
+                <Button className="text-[#E79057] bg-white border border-[#E79057] hover:bg-[#FFF9F5]">
+                    Add New Page
+                </Button>
+                {/* TODO 목록 UI 하나 */}
+                <div className="flex flex-col mt-4 gap-2">
+                    <small className="text-sm font-medium leading-none text-[#A6A6A6]">
+                        9Diin의 TODO-BOARD
+                    </small>
+                    <ul className="flex flex-col">
+                        <li className="flex items-center gap-2 py-2 px-[10px] bg-[#F5F5F5] rounded-sm text-sm">
+                            <div className="h-[6px] w-[6px] rounded-full bg-[#00F38D]"></div>
+                            Enter Title
+                        </li>
+                        <li className="flex items-center gap-2 py-2 px-[10px] bg-[#F5F5F5] rounded-sm text-sm">
+                            <div className="h-[6px] w-[6px] rounded-full bg-[#00F38D]"></div>
+                            Enter Title
+                        </li>
+                    </ul>
+                </div>
+            </aside>
             <main className="page__main">
                 <div className={styles.header}>
                     {/* 제목 입력 Input 섹션 */}
@@ -25,3 +48,6 @@ function BoardPage() {
 }
 
 export default BoardPage;
+
+//There is no board yet.
+//Click the button and start flashing!
